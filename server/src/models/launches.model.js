df37a81244caaf0d0427ff6484aaeb1ed61e9906
Bakare Damilola <bdamilola00@gmail.com>
2023-08-10 22:19:57 +0100

    added a database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,3 +1,5 @@
+// const launches = require("./launches.mongo");
+
 const launches = new Map();
 const songs = new Map
 let latestFlightNumber = 100;
@@ -8,7 +10,7 @@ const launch = {
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   target: "Kepler-142 b",
-  customer: ["ZTM", "NASA"],
+  customers: ["ZTM", "NASA"],
   upcoming: true,
   success: true,
 };
@@ -39,7 +41,7 @@ function addNewLaunch(launch) {
   launches.set(
     latestFlightNumber,
     Object.assign(launch, {
-      customer: ['Zero to Mastery', 'NASA'],
+      customers: ['Zero to Mastery', 'NASA'],
       flightNumber: latestFlightNumber,
       upcoming:true,
       success:true,
--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = "mongodb+srv://nasa-api:Einstein5914@nasacluster.50aizao.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://nasa-api:Einstein5914@nasacluster.50aizao.mongodb.net/nasa?retryWrites=true&w=majority" ;
 
 const server = http.createServer(app);
 
