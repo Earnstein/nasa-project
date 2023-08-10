c1a9aa763f696e9f6450013f0c06a3ae9c4acf54
Bakare Damilola <bdamilola00@gmail.com>
2023-08-10 23:34:42 +0100

    changed morgan log to 'dev'

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
