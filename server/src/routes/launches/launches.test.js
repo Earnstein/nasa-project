180c4d422844f74c180514b16d3b48e742cdd94a
Bakare Damilola <bdamilola00@gmail.com>
2023-08-25 22:02:46 +0100

    modified test file to use new endpoint

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -11,10 +11,10 @@ describe("Launches API", () => {
       await mongoDisconnect();
     })
 
-  describe("Test GET /launches", () => {
+  describe("Test GET launches", () => {
     test("It should respond with  200 success", async () => {
       const response = await request(app)
-        .get("/launches")
+        .get("/v1/launches")
         .expect("Content-Type", /json/)
         .expect(200);
     });
@@ -40,7 +40,7 @@ describe("Launches API", () => {
     };
     test("It should respond with 201 created", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(completeLaunchData)
         .expect("Content-Type", /json/)
         .expect(201);
@@ -52,7 +52,7 @@ describe("Launches API", () => {
     });
     test("It should catch missing properties", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithoutDate)
         .expect("Content-Type", /json/)
         .expect(400);
@@ -62,7 +62,7 @@ describe("Launches API", () => {
     });
     test("It should catch invalid dates", async () => {
       const response = await request(app)
-        .post("/launches")
+        .post("/v1/launches")
         .send(launchDataWithInvalidDate)
         .expect("Content-Type", /json/)
         .expect(400);
